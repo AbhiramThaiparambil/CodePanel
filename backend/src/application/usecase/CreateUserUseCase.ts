baec58f0bf7b9db180f75afb58b43526ce11a1b6
AbhiramThaiparambil <abhiramthaipparambil@gmail.com>
2026-03-24 15:00:23 +0530

    feat(backend): implement CreateUser use case with DTO mapping and interface

--- backend/src/application/usecase/CreateUserUseCase.ts
+++ backend/src/application/usecase/CreateUserUseCase.ts
@@ -1,10 +1,13 @@
+import { inject, injectable } from "tsyringe";
+import { TOKENS } from "../../constant/tokens";
 import { IUser } from "../domin/IUser";
 import { IUserRepository } from "../domin/IUserRepository";
 import { CreateUserDTO } from "../dto/CreateUserDTO";
 import { ICreateUserUseCase } from "./ICreateUserUseCase";
 
+@injectable()
 export class CreateUserUseCase implements ICreateUserUseCase {
-    constructor(private readonly userRepository: IUserRepository) { }
+    constructor(@inject(TOKENS.IUserRepository) private readonly userRepository: IUserRepository) { }
 
     async execute(dto: CreateUserDTO): Promise<IUser> {
         const existing = await this.userRepository.findByEmail(dto.email);
