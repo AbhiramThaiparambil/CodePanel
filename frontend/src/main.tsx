b116da3ec1ca873cb03435a8c51535e814ef20cb
AbhiramThaiparambil <abhiramthaipparambil@gmail.com>
2026-03-24 23:46:27 +0530

    feat(frontend): set up Tailwind CSS + DaisyUI + React Router + Lucide React

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,8 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import './app.css'
 import App from './App.tsx'
 import { ClerkProvider } from '@clerk/react'
+import { BrowserRouter } from 'react-router'
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
 
 if (!PUBLISHABLE_KEY) {
@@ -11,8 +12,10 @@ if (!PUBLISHABLE_KEY) {
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
-      <App />
-    </ClerkProvider>
+    <BrowserRouter>
+      <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+        <App />
+      </ClerkProvider>
+    </BrowserRouter>
   </StrictMode>,
 )
