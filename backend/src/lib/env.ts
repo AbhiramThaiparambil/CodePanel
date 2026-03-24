87c250c75ff02daa62e75160e2c336f0e45f2d46
AbhiramThaiparambil <abhiramthaipparambil@gmail.com>
2026-03-24 22:43:42 +0530

    feat(backend): add custom types to Express Request (create express.d.ts)

--- backend/src/lib/env.ts
+++ backend/src/lib/env.ts
@@ -4,5 +4,8 @@ dotenv.config();
 export const env = {
     PORT: process.env.PORT,
     DB_URL: process.env.DB_URL,
-    INNGEST_SIGNING_KEY: process.env.INNGEST_SIGNING_KEY
+    INNGEST_SIGNING_KEY: process.env.INNGEST_SIGNING_KEY,
+    STREAM_API_KEY: process.env.STREAM_API_KEY,
+    STREAM_SECREt: process.env.STREAM_SECREt
+
 };
